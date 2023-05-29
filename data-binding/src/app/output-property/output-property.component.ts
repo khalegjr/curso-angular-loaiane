0ad36d9aa54d9ce27629404811c113fa282e7985
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-05-29 10:16:05 -0300

    cria componente output-property

---
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-output-property',
+  templateUrl: './output-property.component.html',
+  styleUrls: ['./output-property.component.scss']
+})
+export class OutputPropertyComponent {
+
+}
