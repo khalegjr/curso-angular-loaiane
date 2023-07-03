b57763228d176316f2c713aa837c4ca0635754ae
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-03 12:24:39 -0300

    aula 36: criando uma diretiva de estrutura (ngElse)

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -14,9 +14,10 @@ import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 import { HighlightDirective } from './shared/highlight.directive';
+import { NgElseDirective } from './shared/ng-else.directive';
 
 @NgModule({
-  declarations: [AppComponent, DataBindingComponent, InputPropertyComponent, OutputPropertyComponent, CicloComponent, ExemploNgContentComponent, FundoAmareloDirective, DiretivasCustomizadasComponent, HighlightMouseDirective, HighlightDirective],
+  declarations: [AppComponent, DataBindingComponent, InputPropertyComponent, OutputPropertyComponent, CicloComponent, ExemploNgContentComponent, FundoAmareloDirective, DiretivasCustomizadasComponent, HighlightMouseDirective, HighlightDirective, NgElseDirective],
   imports: [BrowserModule, AppRoutingModule, NgbModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- data-binding/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
+++ data-binding/src/app/diretivas-customizadas/diretivas-customizadas.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretivas-customizadas',
   templateUrl: './diretivas-customizadas.component.html',
-  styleUrls: ['./diretivas-customizadas.component.scss']
+  styleUrls: ['./diretivas-customizadas.component.scss'],
 })
 export class DiretivasCustomizadasComponent {
+  mostrarCursos = false;
 
+  onMostrarCursos() {
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
