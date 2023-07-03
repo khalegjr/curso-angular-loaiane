cf0f77c08ab6c4177e7517f42418594cea158cb2
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-03 13:11:49 -0300

    aula 39: injeção de dependências

--- 05-servicos/src/app/app.module.ts
+++ 05-servicos/src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { CursosService } from './cursos/cursos.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CursosComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, CursosComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [CursosService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
