7b0c8f6d76d9c46a19a6767ddf4b82658f95ea36
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-07 14:56:16 -0300

    aula 45: aplicando locale nos pipes

--- 06-pipes/src/app/app.module.ts
+++ 06-pipes/src/app/app.module.ts
@@ -1,22 +1,32 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localePT from '@angular/common/locales/pt';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { SettingsService } from './settings.service';
+
+registerLocaleData(localePT);
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplosPipesComponent,
-    CamelCasePipe
+  declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    // {
+    //   provide: LOCALE_ID,
+    //   useValue: 'pt-BR',
+    // },
+    SettingsService,
+    {
+      provide: LOCALE_ID,
+      deps: [SettingsService],
+      useFactory: (settingsService: SettingsService) =>
+        settingsService.getLocale(),
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
