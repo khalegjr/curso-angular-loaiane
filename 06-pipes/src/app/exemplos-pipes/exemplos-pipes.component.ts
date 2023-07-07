ace647fa269b614ddedfa742ae765d5c0a8f33e9
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-07 15:48:10 -0300

    aula 46: criando um pipe puro

--- 06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ 06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-exemplos-pipes',
@@ -14,4 +14,12 @@ export class ExemplosPipesComponent {
     dataLancamento: new Date(2016, 5, 23),
     url: 'http://a.co/glgjpRP',
   };
+
+  livros: string[] = ['Angular', 'React', 'Java'];
+  @Input() filtro!: string;
+
+  addCurso(valor: string) {
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
 }
