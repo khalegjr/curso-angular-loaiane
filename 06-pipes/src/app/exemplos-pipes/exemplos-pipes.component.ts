82f8308f7bdd2d1b859dc62243b9f1c7e796e912
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-07 11:56:18 -0300

    aula43: pipes - usando pipes, parâmetros e pipes aninhados

---
+++ 06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplos-pipes',
+  templateUrl: './exemplos-pipes.component.html',
+  styleUrls: ['./exemplos-pipes.component.scss'],
+})
+export class ExemplosPipesComponent {
+  livro: any = {
+    titulo: 'Aprendendo Javascript',
+    rating: 4.54321,
+    numeroPaginas: 314,
+    preco: 44.99,
+    dataLancamento: new Date(2016, 5, 23),
+    url: 'http://a.co/glgjpRP',
+  };
+}
