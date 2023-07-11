df662facbb156776bbb4faf32f44a0c840f7e0cb
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-11 15:31:01 -0300

    aula 62: rotas: carregamento sob demanda (lazy loading)

--- 07-rotas/src/app/app.module.ts
+++ 07-rotas/src/app/app.module.ts
@@ -7,10 +7,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AlunosModule } from './alunos/alunos.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
@@ -25,8 +23,6 @@ import { LoginComponent } from './login/login.component';
     MatButtonModule,
     MatInputModule,
     MatListModule,
-    CursosModule,
-    AlunosModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
