f329d22ff1d6ddd5cc3f2aefe1d49a89c5c8e694
Júnior Souza - Khaled <junior.souza@ti9.com.br>
2023-07-20 10:29:19 -0300

    aula 121: cria ambientes de desenvolvimento e produção e uso de variável de ambiente

--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Curso } from './curso';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CursosService {
-  private readonly API = 'http://localhost:3000/cursos';
+  private readonly API = `${environment.API}cursos`;
 
   constructor(private http: HttpClient) {}
 
